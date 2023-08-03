7306fc7ad24739da2bdbf5a4787a9aa5b221c0be
YICKCT <139414443+YICKCT@users.noreply.github.com>
2023-08-03 12:40:23 +1000

    ship

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,5 @@
 from GameFrame import RoomObject
+import pygame
 
 class Ship(RoomObject):
     """
@@ -15,4 +16,31 @@ class Ship(RoomObject):
         image = self.load_image("Ship.png")
         self.set_image(image,100,100)
 
+        # register events
+        self.handle_key_events = True
         
+    def key_pressed(self, key):
+        """
+        Respond to keypress up and down
+        """
+        
+        if key[pygame.K_w]:
+            self.y -= 15
+        elif key[pygame.K_s]:
+            self.y += 15
+        elif key[pygame.K_a]:
+            self.x -= 15
+        elif key[pygame.K_d]:
+            self.x += 15
+
+    def keep_in_room(self):
+        """
+        Keeps the ship inside the room
+        """
+        if self.y < 0:
+            self.y = 0
+        elif self.y + self.height> Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+        
+
+    
