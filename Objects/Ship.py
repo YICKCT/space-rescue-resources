29484e3229b2c294b63287d6027a9fecc6b16e05
YICKCT <139414443+YICKCT@users.noreply.github.com>
2023-08-08 14:15:31 +1000

    zork & border

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 
 class Ship(RoomObject):
@@ -25,13 +25,13 @@ class Ship(RoomObject):
         """
         
         if key[pygame.K_w]:
-            self.y -= 15
+            self.y -= 100
         elif key[pygame.K_s]:
-            self.y += 15
+            self.y += 100
         elif key[pygame.K_a]:
-            self.x -= 15
+            self.x -= 100
         elif key[pygame.K_d]:
-            self.x += 15
+            self.x += 100
 
     def keep_in_room(self):
         """
@@ -41,6 +41,16 @@ class Ship(RoomObject):
             self.y = 0
         elif self.y + self.height> Globals.SCREEN_HEIGHT:
             self.y = Globals.SCREEN_HEIGHT - self.height
+        elif self.x < 0:
+            self.x = 0 
+        elif self.x + self.width > Globals.SCREEN_WIDTH:
+            self.x = Globals.SCREEN_WIDTH - self.width
+
+    def step(self):
+        """
+        Determine what happens to the Ship on each click of the game clock
+        """
+        self.keep_in_room()
         
 
     
---
+++ Objects/Zork.py
@@ -0,0 +1,16 @@
+from GameFrame import RoomObject
+
+class Zork(RoomObject):
+    """
+    A class for the game's antagoist
+    """
+    def __init__(self, room, x, y):
+        """
+        Initialise the Boss object
+        """
+        # include attributes and methods from RoomObject
+        RoomObject.__init__(self, room, x, y)
+        
+        # set image
+        image = self.load_image("Zork.png")
+        self.set_image(image,135,165)
